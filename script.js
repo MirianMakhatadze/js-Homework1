4660debe87cec69149eaba7f52690afb1589803f
MirianMakhatadze <mirikomaxatadze@gmail.com>
2023-01-13 17:44:19 +0400

    added index and script

---
+++ script.js
@@ -0,0 +1,25 @@
+// problem 1
+// შექმენით რამდენიმე ცვლადი სადაც შეინახავთ თქვენს სახელს, გვარს, სქესს და დაბადების წელს.
+let name = "Mirian"
+let lastName = "Makhatadze"
+let gender = "Male"
+let yearOfBirth = "2003" 
+
+console.log(name)
+console.log(lastName)
+console.log(gender)
+console.log(yearOfBirth)
+// problem 2
+// რამდენ ექსფრეშენს ხედავთ მოცემულ კოდში?
+
+// (100 - (10+15)/5)*3
+
+// 9 expressions
+
+// problem 3
+// შეინახეთ ცვლადში და შემდეგ კონსოლში დაბეჭდეთ რას უდრის 101-ის 2-ზე გაყოფისას მიღებული ნაშთი (კონსოლში მნიშვნელობის გამოსატანად ვიყენებთ console.log() ფუნქციას).
+
+let x = "101"
+let y = "2"
+
+console.log(x%y)
